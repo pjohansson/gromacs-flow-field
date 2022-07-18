134d4aeafd420fb134c773e19515183921b77615
Petter Johansson <pjohansson@univ-pau.fr>
2022-07-18 19:44:19 +0200

    [flow] Improve structure in flow field source code
    We introduce sections for all important sections to make it easier to
    navigate. A constant work in progress.

--- src/gromacs/flow/flow_field.cpp
+++ src/gromacs/flow/flow_field.cpp
@@ -23,204 +23,14 @@
 #include "gromacs/utility/smalloc.h"
 
 #include "flow_field.h"
+#include "utils.h"
 
 namespace flow
 {
 
-/*! \brief Get the number of groups in User1
-
-    This is slightly complicated by how Gromacs adds a "rest" group
-    to the array of names if the other groups do not add up to all
-    atoms in the system. Thus, we detect if the final group is called
-    exactly "rest" and if so do not count it as one of the groups. */
-static size_t
-get_num_groups(const SimulationGroups *groups)
-{
-    size_t num_groups = 0;
-
-    for (const auto global_group_index
-         : groups->groups[SimulationAtomGroupType::User1])
-    {
-        const auto name = groups->groupNames[global_group_index];
-
-        if (strncmp(*name, "rest", 8) == 0)
-        {
-            break;
-        }
-
-        num_groups++;
-    }
-
-    return num_groups;
-}
-
-
-FlowData
-init_flow_container(const int               nfile,
-                    const t_filenm          fnm[],
-                    const t_inputrec       *ir,
-                    const SimulationGroups *groups,
-                    const t_state          *state)
-{
-    const auto step_collect = static_cast<uint64_t>(ir->userint1);
-    auto step_output = static_cast<uint64_t>(ir->userint2);
-
-    const auto nx = ir->userint3;
-    const auto nz = ir->userint4;
-
-    // Control userargs, although this should be done during pre-processing
-    if (nx <= 0 || nz <= 0)
-    {
-        gmx_fatal(FARGS,
-                  "Number of bins along x (userint3 = %d) and z (userint4 = %d) "
-                  "for flow data calculation and output must be larger than 0.",
-                  nx, nz);
-    }
-
-    if (step_collect <= 0 || step_output <= 0)
-    {
-        gmx_fatal(FARGS,
-                  "Number of steps that elapse between collection (userint1 = %lu) "
-                  "and output (userint2 = %lu) of flow data must be larger than 0.",
-                  step_collect, step_output);
-    }
-    else if (step_collect > step_output)
-    {
-        gmx_fatal(FARGS,
-                  "Number of steps elapsing between output (userint2 = %lu) "
-                  "must be larger than steps between collection (userint1 = %lu).",
-                  step_output, step_collect);
-    }
-    else if (step_output % step_collect != 0)
-    {
-        const auto new_step_output = static_cast<uint64_t>(
-            round(step_output / step_collect) * step_collect
-        );
-
-        gmx_warning("Steps for outputting flow data (userint2 = %lu) not "
-                    "multiple of steps for collecting (userint1 = %lu). "
-                    "Setting number of steps that elapse between output to %lu.",
-                    step_output, step_collect, new_step_output);
-
-        step_output = new_step_output;
-    }
-
-    // Get name base of output datamaps by stripping the extension and dot (.)
-    std::string fnbase = opt2fn("-flow", nfile, fnm);
-
-    const int ext_length = static_cast<int>(strlen(ftp2ext(efDAT)));
-    const int base_length = static_cast<int>(fnbase.size()) - ext_length - 1;
-
-    if (base_length > 0)
-    {
-        fnbase.resize(static_cast<size_t>(base_length));
-    }
-
-    // If more than one group is selected for output, collect them to do separate
-    // collection for each individual group (as well as them all combined)
-    //
-    // Get the number of selected groups, subtract 1 because "rest" is always present
-    // const size_t num_groups = groups->grps[egcUser1].nr - 1;
-    const size_t num_groups = get_num_groups(groups);
-
-    std::vector<std::string> group_names;
-
-    if (num_groups > 1)
-    {
-        for (size_t i = 0; i < num_groups; ++i)
-        {
-            const auto global_group_index
-                = groups->groups[SimulationAtomGroupType::User1].at(i);
-
-            const char *name = *groups->groupNames[global_group_index];
-            group_names.push_back(std::string(name));
-        }
-    }
-
-    return FlowData(
-        fnbase, group_names, nx, nz, state->box, step_collect, step_output
-    );
-}
-
-
-void
-print_flow_collection_information(const FlowData       &flowcr,
-                                  const double          dt,
-                                  const gmx::MDLogger  &mdlog)
-{
-    // Log to warning level, which prints both to md.log and stdout
-    // (info level only writes to md.log)
-
-    GMX_LOG(mdlog.warning)
-        .asParagraph()
-        .appendText("************************************\n")
-        .appendText("* FLOW DATA COLLECTION INFORMATION *\n")
-        .appendText("************************************");
-
-    GMX_LOG(mdlog.warning)
-        .asParagraph()
-        .appendText("Flow field collection frequency:\n")
-        .appendTextFormatted(
-            "  Collect: %g ps (every %lu steps).\n",
-            flowcr.step_collect * dt, flowcr.step_collect
-        )
-        .appendTextFormatted(
-            "  Output:  %g ps (every %lu steps).",
-            flowcr.step_output * dt, flowcr.step_output
-        );
-
-    GMX_LOG(mdlog.warning)
-        .asParagraph()
-        .appendText("Flow field grid information:\n")
-        .appendTextFormatted(
-            "  Shape:   %lu x %lu (along x and z)\n",
-            flowcr.flow_field.nx(), flowcr.flow_field.nz()
-        )
-        .appendTextFormatted(
-            "  Spacing: %g x %g nm^2",
-            flowcr.flow_field.dx(), flowcr.flow_field.dz()
-        );
-
-    GMX_LOG(mdlog.warning)
-        .asParagraph()
-        .appendTextFormatted(
-            "Writing full flow data to files "
-            "with base '%s_00001.dat' (...).",
-            flowcr.flow_field.fnbase.c_str()
-        );
-
-    if (!flowcr.group_data.empty())
-    {
-        GMX_LOG(mdlog.warning)
-            .asParagraph()
-            .appendText(
-                "Multiple groups selected for flow output. Will collect "
-                "individual flow data for each group individually in "
-                "addition to the combined field:\n"
-            );
-
-        for (const auto& group : flowcr.group_data)
-        {
-            GMX_LOG(mdlog.warning)
-                .appendTextFormatted(
-                    "  %s -> '%s_00001.dat' (...)\n",
-                    group.name.c_str(),
-                    group.fnbase.c_str()
-                );
-        }
-    }
-
-    GMX_LOG(mdlog.warning)
-        .asParagraph()
-        .appendText("Have a nice day.");
-
-    GMX_LOG(mdlog.warning)
-        .asParagraph()
-        .appendText("****************************************\n")
-        .appendText("* END FLOW DATA COLLECTION INFORMATION *\n")
-        .appendText("****************************************");
-}
-
+/*******************
+ * DATA COLLECTION *
+ *******************/
 
 static void
 add_flow_to_bin(flow::Bin     &bin,
@@ -303,6 +113,10 @@ collect_flow_data(flow::FlowData         &flowcr,
     ++flowcr.num_samples;
 }
 
+
+/**********
+ * OUTPUT *
+ **********/
 
 struct FlowBinData {
     float mass,
@@ -425,6 +239,49 @@ add_bin_if_non_empty(GroupOutput       &data,
 }
 
 
+static FlowFieldOutput
+get_average_flow_data(FlowData &flowcr)
+{
+    FlowFieldOutput output(flowcr);
+
+    for (size_t ix = 0; ix < flowcr.flow_field.nx(); ++ix)
+    {
+        for (size_t iz = 0; iz < flowcr.flow_field.nz(); ++iz)
+        {
+            const auto bin = flowcr.flow_field.at(ix, 0, iz);
+            const auto bin_data = calc_values_in_bin(bin, flowcr.num_samples);
+
+            add_bin_if_non_empty(
+                output.all_groups, ix, iz, flowcr.bin_volume, bin_data
+            );
+
+            auto group_output = output.individual_groups.begin();
+            auto group_data = flowcr.group_data.cbegin();
+
+            while (
+                (group_output != output.individual_groups.end())
+                && (group_data != flowcr.group_data.cend())
+            )
+            {
+                const auto bin = (*group_data).at(ix, 0, iz);
+                const auto group_bin_data = calc_values_in_bin(
+                    bin, flowcr.num_samples
+                );
+
+                add_bin_if_non_empty(
+                    *group_output, ix, iz, flowcr.bin_volume, group_bin_data
+                );
+
+                ++group_output;
+                ++group_data;
+            }
+        }
+    }
+
+    return output;
+}
+
+
 static void
 write_header(FILE         *fp,
              const size_t  nx,
@@ -498,6 +355,32 @@ write_flow_data(const GroupOutput &output,
 
 
 static void
+output_flow_data(const FlowFieldOutput &output,
+                 const uint64_t         current_step,
+                 const uint64_t         step_output)
+{
+    const auto file_index = static_cast<size_t>(current_step / step_output);
+
+    write_flow_data(
+        output.all_groups, file_index,
+        output.nx, output.nz, output.dx, output.dz
+    );
+
+    for (const auto& group_data : output.individual_groups)
+    {
+        write_flow_data(
+            group_data, file_index,
+            output.nx, output.nz, output.dx, output.dz
+        );
+    }
+}
+
+
+/*********************
+ * MPI COMMUNICATION *
+ *********************/
+
+static void
 mpi_collect_flow_data_on_master(FlowData        &flowcr,
                                 const t_commrec *cr)
 {
@@ -521,68 +404,172 @@ mpi_collect_flow_data_on_master(FlowData        &flowcr,
 }
 
 
-static FlowFieldOutput
-get_average_flow_data(FlowData &flowcr)
+/********************
+ * PUBLIC FUNCTIONS *
+ ********************/
+
+FlowData
+init_flow_container(const int               nfile,
+                    const t_filenm          fnm[],
+                    const t_inputrec       *ir,
+                    const SimulationGroups *groups,
+                    const t_state          *state)
 {
-    FlowFieldOutput output(flowcr);
+    const auto step_collect = static_cast<uint64_t>(ir->userint1);
+    auto step_output = static_cast<uint64_t>(ir->userint2);
 
-    for (size_t ix = 0; ix < flowcr.flow_field.nx(); ++ix)
+    const auto nx = ir->userint3;
+    const auto nz = ir->userint4;
+
+    // Control userargs, although this should be done during pre-processing
+    if (nx <= 0 || nz <= 0)
     {
-        for (size_t iz = 0; iz < flowcr.flow_field.nz(); ++iz)
+        gmx_fatal(FARGS,
+                  "Number of bins along x (userint3 = %d) and z (userint4 = %d) "
+                  "for flow data calculation and output must be larger than 0.",
+                  nx, nz);
+    }
+
+    if (step_collect <= 0 || step_output <= 0)
+    {
+        gmx_fatal(FARGS,
+                  "Number of steps that elapse between collection (userint1 = %lu) "
+                  "and output (userint2 = %lu) of flow data must be larger than 0.",
+                  step_collect, step_output);
+    }
+    else if (step_collect > step_output)
+    {
+        gmx_fatal(FARGS,
+                  "Number of steps elapsing between output (userint2 = %lu) "
+                  "must be larger than steps between collection (userint1 = %lu).",
+                  step_output, step_collect);
+    }
+    else if (step_output % step_collect != 0)
+    {
+        const auto new_step_output = static_cast<uint64_t>(
+            round(step_output / step_collect) * step_collect
+        );
+
+        gmx_warning("Steps for outputting flow data (userint2 = %lu) not "
+                    "multiple of steps for collecting (userint1 = %lu). "
+                    "Setting number of steps that elapse between output to %lu.",
+                    step_output, step_collect, new_step_output);
+
+        step_output = new_step_output;
+    }
+
+    // Get name base of output datamaps by stripping the extension and dot (.)
+    std::string fnbase = opt2fn("-flow", nfile, fnm);
+
+    const int ext_length = static_cast<int>(strlen(ftp2ext(efDAT)));
+    const int base_length = static_cast<int>(fnbase.size()) - ext_length - 1;
+
+    if (base_length > 0)
+    {
+        fnbase.resize(static_cast<size_t>(base_length));
+    }
+
+    // If more than one group is selected for output,
+    // collect them to do separate collection for each
+    // individual group (as well as them all combined)
+    const size_t num_groups = get_num_groups(groups);
+
+    std::vector<std::string> group_names;
+
+    if (num_groups > 1)
+    {
+        for (size_t i = 0; i < num_groups; ++i)
         {
-            const auto bin = flowcr.flow_field.at(ix, 0, iz);
-            const auto bin_data = calc_values_in_bin(bin, flowcr.num_samples);
+            const auto global_group_index
+                = groups->groups[SimulationAtomGroupType::User1].at(i);
 
-            add_bin_if_non_empty(
-                output.all_groups, ix, iz, flowcr.bin_volume, bin_data
-            );
-
-            auto group_output = output.individual_groups.begin();
-            auto group_data = flowcr.group_data.cbegin();
-
-            while (
-                (group_output != output.individual_groups.end())
-                && (group_data != flowcr.group_data.cend())
-            )
-            {
-                const auto bin = (*group_data).at(ix, 0, iz);
-                const auto group_bin_data = calc_values_in_bin(
-                    bin, flowcr.num_samples
-                );
-
-                add_bin_if_non_empty(
-                    *group_output, ix, iz, flowcr.bin_volume, group_bin_data
-                );
-
-                ++group_output;
-                ++group_data;
-            }
+            const char *name = *groups->groupNames[global_group_index];
+            group_names.push_back(std::string(name));
         }
     }
 
-    return output;
+    return FlowData(
+        fnbase, group_names, nx, nz, state->box, step_collect, step_output
+    );
 }
 
 
-static void
-output_flow_data(const FlowFieldOutput &output,
-                 const uint64_t         current_step,
-                 const uint64_t         step_output)
+void
+print_flow_collection_information(const FlowData       &flowcr,
+                                  const double          dt,
+                                  const gmx::MDLogger  &mdlog)
 {
-    const auto file_index = static_cast<size_t>(current_step / step_output);
+    // Log to warning level, which prints both to md.log and stdout
+    // (info level only writes to md.log)
 
-    write_flow_data(
-        output.all_groups, file_index,
-        output.nx, output.nz, output.dx, output.dz
-    );
+    GMX_LOG(mdlog.warning)
+        .asParagraph()
+        .appendText("************************************\n")
+        .appendText("* FLOW DATA COLLECTION INFORMATION *\n")
+        .appendText("************************************");
 
-    for (const auto& group_data : output.individual_groups)
-    {
-        write_flow_data(
-            group_data, file_index,
-            output.nx, output.nz, output.dx, output.dz
+    GMX_LOG(mdlog.warning)
+        .asParagraph()
+        .appendText("Flow field collection frequency:\n")
+        .appendTextFormatted(
+            "  Collect: %g ps (every %lu steps).\n",
+            flowcr.step_collect * dt, flowcr.step_collect
+        )
+        .appendTextFormatted(
+            "  Output:  %g ps (every %lu steps).",
+            flowcr.step_output * dt, flowcr.step_output
         );
+
+    GMX_LOG(mdlog.warning)
+        .asParagraph()
+        .appendText("Flow field grid information:\n")
+        .appendTextFormatted(
+            "  Shape:   %lu x %lu (along x and z)\n",
+            flowcr.flow_field.nx(), flowcr.flow_field.nz()
+        )
+        .appendTextFormatted(
+            "  Spacing: %g x %g nm^2",
+            flowcr.flow_field.dx(), flowcr.flow_field.dz()
+        );
+
+    GMX_LOG(mdlog.warning)
+        .asParagraph()
+        .appendTextFormatted(
+            "Writing full flow data to files "
+            "with base '%s_00001.dat' (...).",
+            flowcr.flow_field.fnbase.c_str()
+        );
+
+    if (!flowcr.group_data.empty())
+    {
+        GMX_LOG(mdlog.warning)
+            .asParagraph()
+            .appendText(
+                "Multiple groups selected for flow output. Will collect "
+                "individual flow data for each group individually in "
+                "addition to the combined field:\n"
+            );
+
+        for (const auto& group : flowcr.group_data)
+        {
+            GMX_LOG(mdlog.warning)
+                .appendTextFormatted(
+                    "  %s -> '%s_00001.dat' (...)\n",
+                    group.name.c_str(),
+                    group.fnbase.c_str()
+                );
+        }
     }
+
+    GMX_LOG(mdlog.warning)
+        .asParagraph()
+        .appendText("Have a nice day.");
+
+    GMX_LOG(mdlog.warning)
+        .asParagraph()
+        .appendText("****************************************\n")
+        .appendText("* END FLOW DATA COLLECTION INFORMATION *\n")
+        .appendText("****************************************");
 }
 
 
